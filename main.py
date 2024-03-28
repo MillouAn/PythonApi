e7be6e267501c7b979364c4bfe2fdb4487fad580
MillouAn <anastasia-semenova97@mail.ru>
2024-03-28 20:02:48 +0300

    test commit

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import requests
-from json.decoder import JSONDecodeError
 
+response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+first_response = response.history[0]
+second_response = response
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
-
-try:
-    parsed_response_text = response.json()
-    print(parsed_response_text)
-except JSONDecodeError:
-    print("response is not a JSON format")
+print(first_response.url)
+print(second_response.url)
